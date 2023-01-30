249758de56190aef5c1462d207dfd3844645f22a
Rahul Gupta <rahuljandial2@gmail.com>
2023-01-30 21:17:16 +0530

    Editting a message note works properly now
    - Previous message is displayed over the inputs and also get copied in the input so the user can update it properly

--- lib/widgets/my_drawer.dart
+++ lib/widgets/my_drawer.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:organise_me/screens/settings_screen.dart';
 
 class MyDrawer extends StatelessWidget {
   const MyDrawer({super.key});
@@ -9,8 +10,16 @@ class MyDrawer extends StatelessWidget {
     return Drawer(
       child: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
+            ListTile(
+              leading: const Icon(Icons.settings),
+              title: const Text("Settings"),
+              onTap: () => Navigator.of(context).push(
+                MaterialPageRoute(builder: (context) => const SettingsScreen()),
+              ),
+            ),
+            const Divider(),
             FilledButton(
               onPressed: () => FirebaseAuth.instance.signOut(),
               child: const Text('Sign Out'),
